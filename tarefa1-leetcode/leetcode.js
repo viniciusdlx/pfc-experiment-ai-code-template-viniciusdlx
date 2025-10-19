58e63a18baccecef96a55cf0dae66f104eb76358
viniciusdlx <viniciusdelimaxavier@gmail.com>
2025-10-19 17:26:07 -0300

    feat: enhance valid parentheses validation with detailed error reporting

--- tarefa1-leetcode/leetcode.js
+++ tarefa1-leetcode/leetcode.js
@@ -36,7 +36,29 @@ const map = {
   "]": "[",
 };
 
-function isValid(s) {
+const isCorresponding = [
+  ["(", ")"],
+  ["[", "]"],
+  ["{", "}"],
+];
+
+function getErroMessage({ openingCharacter, closingCharacter }) {
+  if (!openingCharacter && closingCharacter) {
+    return "Parêntese fechado";
+  }
+
+  if (!closingCharacter) {
+    return "não foi fechado";
+  }
+
+  if (!map[closingCharacter] !== openingCharacter) {
+    return "não corresponde";
+  }
+}
+
+function isValid(s, returnDetails = false) {
+  let isValidString = true;
+
   // se string for vazia ele retorna true
   if (!s) {
     return true;
@@ -44,29 +66,93 @@ function isValid(s) {
 
   // cria uma variavel de stack dos caracteres especificos
   let stack = [];
+  let charAndPosition = {
+    openingCharacter: "",
+    closingCharacter: "",
+    openingPosition: 0,
+    closingPosition: 0,
+  };
 
   // faz um for pra verificar cada caracter da string especificada pelo tamanho dela
   for (let i = 0; i < s.length; i++) {
     // verifica cada caracter se ele contem uma abertura
     if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
       // se houver ele coloca na variavel de stack
+      charAndPosition.openingCharacter = s[i];
+      charAndPosition.openingPosition = i;
       stack.push(s[i]);
     }
     // verifica se o ultimo elemento do stack é de fechamento correspondente ao de abertura
     else if (stack[stack.length - 1] === map[s[i]]) {
       // se for ele remove e ta tudo certo.
       stack.pop();
-    } else return false; // se nao, a string está invalida
+    } else {
+      // ele irá adicionar qual caractere está incorreto
+      charAndPosition.closingCharacter = s[i];
+      // adicionadando qual a posição dele
+      charAndPosition.closingPosition = i;
+      // se nao, a string está invalida
+      isValidString = false;
+      if (returnDetails) {
+        return { isValidString, charAndPosition };
+      } else {
+        return false;
+      }
+    }
   }
 
-  // se houver algo no stack ele não ta com fechamento correto e retorna false
-  // se não houver elementos no stack, todos os caracteres foram fechados corretamente
-  return stack.length ? false : true;
+  if (stack.length) {
+    isValidString = false;
+  }
+
+  if (returnDetails) {
+    // se houver algo no stack ele não ta com fechamento correto e retorna false
+    // se não houver elementos no stack, todos os caracteres foram fechados corretamente
+    return { isValidString, charAndPosition };
+  }
+
+  return isValidString;
 }
 
 function findFirstError(s) {
+  let returnData = {
+    valid: false,
+    error: null,
+    position: 0,
+    character: "",
+  };
+
+  if (!s) {
+    returnData.valid = true;
+    return returnData;
+  }
+
+  const { isValidString, charAndPosition } = isValid(s, true);
+
+  if (isValidString) {
+    returnData.valid = true;
+
+    return returnData;
+  }
+
+  if (!isValidString && !!charAndPosition) {
+    const {
+      openingCharacter,
+      closingCharacter,
+      openingPosition,
+      closingPosition,
+    } = charAndPosition;
+
+    returnData.character = closingCharacter || openingCharacter;
+    returnData.position = closingPosition || openingPosition;
+
+    returnData.error = getErroMessage({ openingCharacter, closingCharacter });
+  }
+
+  returnData.valid = false;
+
   // Implementar aqui
-  return { valid: false, error: null, position: 0, character: "" };
+  return returnData;
 }
 
 module.exports = { isValid, findFirstError };
